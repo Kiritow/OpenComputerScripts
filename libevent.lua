639b35e7ef349bc7fe02542663263781dd677d62
kiritow <1362050620@qq.com>
2017-10-23 18:59:47 +0800

    Update Event Library

--- libevent.lua
+++ libevent.lua
@@ -1,9 +1,24 @@
+require("checkarg")
 local event=require("event")
 
-function AddEventListener(EventString,CallbackFunction)
-    return event.listen(EventString,CallbackFunction)
+function AddEventListener(EventName,CallbackFunction)
+    checkstring(EventName)
+    checkfunction(CallbackFunction)
+    return event.listen(EventName,CallbackFunction)
 end
 
 function RemoveEventListener(ListenerID)
+    checknumber(ListenerID)
     return event.ignore(event.handlers[ListenerID].key,event.handlers[ListenerID].callback)
+end
+
+function WaitEvent(EventName)
+    checkstring(EventName)
+    return event.pull(EventName)
+end
+
+function WaitEventFor(EventName,TimeOut)
+    checkstring(EventName)
+    checknumber(TimeOut)
+    return event.pull(TimeOut,EventName)
 end
