2c5bc2f6494ad35d2b506a1d7278715a4b6af002
kiritow <1362050620@qq.com>
2018-02-13 13:29:59 +0800

    Update system shop
    Stop buying while destroy factory is busy.
    Will block until internal buffer is free

--- SystemShop.lua
+++ SystemShop.lua
@@ -11,6 +11,7 @@ local trans = proxy("transposer")
 local redgate = proxy("redstone")
 
 -- Variables
+local gate_allow = true
 
 -- Functions
 
@@ -30,39 +31,80 @@ end
 
 local function main()
     init()
+
+    local bus=CreateEventBus()
+    bus:listen("bioReader")
+    bus:listen("modem_message")
+    bus:listen("interrupted")
+
     while true do 
         print("Item Price:")
         print("Log (yuan mu) : $0.5")
         print("Other : $0.1")
 
-        local e=WaitMultipleEvent("bioReader","modem_message","interrupted")
+        local e=bus:next()
         if(e.event=="bioReader") then
-            redgate.setOutput(sides.east,15) -- close gate
-            local sum=0
-            local x=trans.getAllStacks(sides.south) -- check item
-            while true do
-                local y=x()
-                if(y==nil) then 
-                    break
-                end
-                if(y.size~=nil) then
-                    if(y.name=="minecraft:log") then
-                        sum=sum+y.size*0.5
-                    else
-                        sum=sum+y.size*0.1
+            if(not gate_allow) then
+                print("Gate is closed. Cannot sell items at present.")
+            else
+                redgate.setOutput(sides.east,15) -- close gate
+                local sum=0
+                local x=trans.getAllStacks(sides.south) -- check item
+                while true do
+                    local y=x()
+                    if(y==nil) then 
+                        break
+                    end
+                    if(y.size~=nil) then
+                        if(y.name=="minecraft:log") then
+                            sum=sum+y.size*0.5
+                        else
+                            sum=sum+y.size*0.1
+                        end
                     end
                 end
+
+                print("Total Price: " .. sum)
+
+                while true do
+                    -- Transfer item
+                    print("Transfering items ... ")
+                    while (trans.transferItem(sides.south,sides.down)) do 
+                    end
+
+                    -- check if left.
+                    x=trans.getAllStacks(sides.south)
+                    local left=false
+                    while true do 
+                        local y=x()
+                        if(y==nil) then 
+                            break
+                        end
+                        if(y.size~=nil) then
+                            left=true
+                            break
+                        end
+                    end
+                    if(left) then
+                        print("Transfer busy. Waiting...")
+                        os.sleep(5)
+                    else
+                        break
+                    end
+                end
+                print("Done")
+
+                redgate.setOutput(sides.east,0) -- open gate
             end
-
-            print("Total Price: " .. sum)
-
-            -- Transfer item
-            print("Transfering items ... ")
-            while (trans.transferItem(sides.south,sides.down)) do 
+        elseif(e.event=="modem_message") then
+            local tb=unserialize(e.data[1])
+            local rate=tb.unit/tb.maxunit*100
+            if(rate>60) then 
+                print("DestroyFactory Busy. Gate is closed for security.")
+                gate_allow=false
+            else
+                gate_allow=true
             end
-            print("Done")
-
-            redgate.setOutput(sides.east,0) -- open gate
         else
             print("Received ctrl+c signal.")
             break
