77b3d323de86e7f913d5edd1d9dc4b549ba7603c
Kiritow <1362050620@qq.com>
2018-12-03 21:46:54 +0800

    Smaller drone bios core. Fix a bug.

--- programs/drone/drone_bios.lua
+++ programs/drone/drone_bios.lua
@@ -1,54 +1,54 @@
-drone_version="Drone v3.3b"
+drone_version="Drone v3.3c"
 drone=component.proxy(component.list("drone")())
 modem=component.proxy(component.list("modem")())
 drone.setStatusText(drone_version .. '\n' .. modem.address)
 modem.open(98)
-handlers={}
-timers={}
-wake_list={}
-handlers["_anything"]={}
+H={["_anything"]={}}
+T={}
+W={}
+local coC,coT,coY,coR,upt,tbI,tbR=coroutine.create,coroutine.running,coroutine.yield,coroutine.resume,computer.uptime(),table.insert,table.remove
 handle_event=function(e)
-    if(handlers[e[1]]) then 
-        for i,tb in pairs(handlers[e[1]]) do 
-            coroutine.resume(tb.co,i,e) 
+    if(H[e[1]]) then 
+        for i,tb in pairs(H[e[1]]) do 
+            coR(tb.co,i,e) 
         end
     end
-    for i,tb in pairs(handlers["_anything"]) do 
-        coroutine.resume(tb.co,i,e)
+    for i,tb in pairs(H["_anything"]) do 
+        coR(tb.co,i,e)
     end
 end
 sleep=function(sec)
-    table.insert(wake_list,{id=0,tm=computer.uptime()+sec,co=coroutine.running()})
-    table.remove(wake_list,coroutine.yield())
+    tbI(W,{id=0,tm=upt()+sec,co=coT()})
+    tbR(W,coY())
 end
 cancel=function(id,wid)
     if(wid) then 
-        table.remove(wake_list,wid) 
+        tbR(W,wid) 
     else 
-        for i,t in ipairs(wake_list) do 
+        for i,t in ipairs(W) do 
             if(t.id==id) then 
-                table.remove(wake_list,i) 
+                tbR(W,i) 
                 break 
             end 
         end
     end
-    if(timers[id]) then 
-        timers[id]=nil 
+    if(T[id]) then 
+        T[id]=nil 
         return true
     else 
         return false 
     end
 end
 timer=function(sec,fn,times)
-    table.insert(timers,{cb=fn,intv=sec,times=times})
-    local id=#timers
-    table.insert(wake_list,{tm=computer.uptime()+sec,id=id,co=coroutine.create(function(wid)
+    tbI(T,{cb=fn,intv=sec,times=times})
+    local id=#T
+    tbI(W,{tm=upt()+sec,id=id,co=coC(function(wid)
         while true do 
             pcall(fn)
-            local tb=timers[id]
+            local tb=T[id]
             if(tb.times<0 or tb.times>1) then 
-                tb.times,tb.tm=tb.times-1,computer.uptime()+tb.intv
-                wid=coroutine.yield()
+                tb.times,tb.tm=tb.times-1,upt()+tb.intv
+                wid=coY()
             else
                 cancel(id,wid)
                 return
@@ -58,75 +58,72 @@ timer=function(sec,fn,times)
     return id
 end
 ignore=function(name,id)
-    if(handlers[name][id]) then 
-        handlers[name][id]=nil 
+    if(H[name][id]) then 
+        H[name][id]=nil 
         return true
     else 
         return false 
     end
 end
 listen=function(name,cb)
-    if(handlers[name]==nil) then 
-        handlers[name]={}
+    if(H[name]==nil) then 
+        H[name]={}
     end
-    table.insert(handlers[name],{co=coroutine.create(function(i,e)
+    tbI(H[name],{co=coC(function(i,e)
         while true do
             local ok,res=pcall(cb,e)
             if(ok and res~=nil and not res) then
                 ignore(name,i)
                 return
             end
-            i,e=coroutine.yield()
+            i,e=coY()
         end
     end)})
-    return #handlers[name]
+    return #H[name]
 end
 wait=function(sec,name)
     if(name==nil and type(sec)=="string") then name=sec sec=nil end
-    sec=sec and computer.uptime()+sec or math.huge
-    local this=coroutine.running()
-    table.insert(wake_list,{id=0,tm=sec,co=this})
+    sec=sec and upt()+sec or math.huge
+    local this=coT()
+    tbI(W,{id=0,tm=sec,co=this})
     name=name or "_anything"
     local id=listen(name,function(e) 
-        coroutine.resume(this,-1,e)
+        coR(this,-1,e)
         return false
     end)
-    local wid,e=coroutine.yield()
+    local wid,e=coY()
     ignore(name,id)
     if(wid==-1) then
-        for i,t in ipairs(wake_list) do 
+        for i,t in ipairs(W) do 
             if(t.co==this) then 
-                table.remove(wake_list,i)
+                tbR(W,i)
                 break
             end
         end
         return e
     else
-        table.remove(wake_list,wid)
+        tbR(W,wid)
         return nil
     end
 end
 listen("modem_message",function(e)
-    local snd=e[3]
-    local tag=e[6]
-    local cmd=e[7]
-    if(tag and tag=='execute_command' and cmd) then 
-        local f,err=load(cmd)
+    if(e[6] and e[6]=='execute_command' and e[7]) then 
+        local f,err=load(e[7])
         if(f) then
-            local ok,err=pcall(f)
-            if(not ok) then 
-                modem.send(snd,99,"CallError: " .. res)
-            else
-                modem.send(snd,99,res)
+            local ok,err=pcall(function()
+                modem.send(e[3],99,f())
+            end)
+            if(not ok) then
+                modem.send(e[3],99,"CallError: " .. err)
             end
         else
-            modem.send(snd,99,"SyntaxError: " .. err)
+            modem.send(e[3],99,"SyntaxError: " .. err)
         end
     end
 end)
 while true do
     local max_wait,max_id=math.huge,0
-    for i,v in ipairs(wake_list) do
+    for i,v in ipairs(W) do
         if(v.tm<max_wait) then 
             max_wait,max_id=v.tm,i
         end
@@ -135,10 +132,10 @@ while true do
     if(max_wait==math.huge) then
         e=table.pack(computer.pullSignal())
     else
-        e=table.pack(computer.pullSignal(max_wait-computer.uptime()))
+        e=table.pack(computer.pullSignal(max_wait-upt()))
     end
     if(e[1]==nil) then
-        coroutine.resume(wake_list[max_id].co,max_id)
+        coR(W[max_id].co,max_id)
     else
         handle_event(e)
     end
