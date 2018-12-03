0c78018707136583d69cbe2fecf9a0c3f2c75202
Kiritow <1362050620@qq.com>
2018-12-03 20:20:19 +0800

    small update

--- teleport_calc.lua
+++ teleport_calc.lua
@@ -1,17 +1,20 @@
 -- Teleport Calculator
+
 local function getEUStr(eu)
     if(eu<1000) then
-        return string.format(
-    elseif(eu<1000*1000) then
-        return '' .. math.ceil(eu/1000) .. 'K,' .. getEUStr(eu%1000)
+        return string.format("%d EU",eu)
+    elseif(eu<1000000) then
+        return string.format("%.2fK EU",eu/1000)
     else
-        return '' .. math.ceil(eu/1000000) .. 'M,' .. getEUStr(eu%1000000)
+        return string.format("%.2fM EU",eu/1000000)
     end
 end
 
-function teleport_calc(dis,item) 
+local function teleport_calc(dis,item) 
     if(item==nil) then item=40 end
     local ret=(1000+100*item)*math.pow(dis+10,0.7)
-    print("Teleport Steve with " .. item .. " items to " .. dis .. "m away needs " .. getEUStr(ret) .. " EU")
+    print("Teleport Steve with " .. item .. " items to " .. dis .. "m away needs " .. getEUStr(ret))
     return ret
 end
+
+return teleport_calc
